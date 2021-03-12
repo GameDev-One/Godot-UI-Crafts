fe2f500e2dbc7c05ac8ef18ab42d8ef678cfe130
Devone Reynolds <gamedevone1@gmail.com>
2021-03-12 09:49:57 -0800

    CardUI Editor Description Added

--- Projects/CardUI/CardUI.gd
+++ Projects/CardUI/CardUI.gd
@@ -4,12 +4,12 @@ onready var Anim: AnimationPlayer = $AnimationPlayer
 onready var Title: Label = $VBoxContainer/Title
 onready var Content: RichTextLabel = $VBoxContainer/HBoxContainer/Contents
 
-export(String) var TitleText: String = "Your Title Goes Here"
-export(String, MULTILINE) var ContentText: String = "Lorem ipsum dolor sit amet"
+export(String) var title_text: String = "Your Title Goes Here"
+export(String, MULTILINE) var content_text: String = "Lorem ipsum dolor sit amet"
 
 func _ready():
-	Title.text = TitleText
-	Content.text = ContentText
+	Title.text = title_text
+	Content.text = content_text
 
 
 func _on_CardUI_mouse_entered():
