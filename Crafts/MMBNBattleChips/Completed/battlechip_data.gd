9988363475e272c1beac1b4d9cf880c94408f5c8
Devone Reynolds <gamedevone1@gmail.com>
2021-03-25 04:23:08 -0700

    Added Icon Image to Battlechip Data

--- Crafts/MMBNBattleChips/Completed/battlechip_data.gd
+++ Crafts/MMBNBattleChips/Completed/battlechip_data.gd
@@ -2,17 +2,18 @@ extends Resource
 class_name BattleChip
 
 enum Element{
-	NORMAL,
+	ELEC,
 	FIRE,
+	NORMAL,
 	WATER,
 	WOOD,
-	ELEC,
 }
 
 export(String) var name = ""
 export(Element) var element = Element.NORMAL
 export(int, 0, 1000, 10) var damage = 0
-export(Texture) var image = null
+export(Texture) var full_image = null
+export(Texture) var icon_image = null
 export(String, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
 				'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 
 				'Z') var rank = 'A'
